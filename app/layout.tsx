57c2c86d869b6e6cd515c340595ff6c51a3a308e
Sibghat Ullah <72342588+Sibghat34@users.noreply.github.com>
2023-10-26 02:09:42 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Rich Text Editor',
+  title: 'Rich Text',
   description: 'Developed by DevAdeelAhmad',
 }
 
